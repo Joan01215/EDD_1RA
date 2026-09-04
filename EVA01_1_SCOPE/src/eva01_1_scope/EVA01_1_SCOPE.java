68ddbe97bb27e65a1ece24f18f675a9ca8298e4e
Joan01215 <joangalaxy07@gmail.com>
2026-09-04 10:56:18 -0600

    Add files via upload

---
+++ EVA01_1_SCOPE/src/eva01_1_scope/EVA01_1_SCOPE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva01_1_scope;
+
+/**
+ *
+ * @author joang
+ */
+public class EVA01_1_SCOPE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
